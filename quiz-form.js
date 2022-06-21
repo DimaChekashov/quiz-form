4e263c43e9e94b614c836ab25402ec56e27aee31
DimaChekashov <foxdima99@gmail.com>
2022-06-21 21:45:38 +0300

    add quiz multipy

--- quiz-form.js
+++ quiz-form.js
@@ -4,11 +4,8 @@ const QUIZ_TYPE = {
     multipy: "multipy",
 };
 
-const quizForm = (formId) => {
+const quizForm = (formId, formType) => {
     const form = document.getElementById(formId);
-    const radio = document.querySelector(
-        "#" + formId + " .quiz-checkbox input[checkbox-success]"
-    );
     const button = document.querySelector("#" + formId + " .quiz-button");
     const alertSuccess = document.querySelector(
         "#" + formId + " .quiz-alert[alert-success]"
@@ -17,21 +14,76 @@ const quizForm = (formId) => {
         "#" + formId + " .quiz-alert[alert-error]"
     );
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
+    switch (formType) {
+        case QUIZ_TYPE.sigle:
+            quizSingle();
+            break;
+        case QUIZ_TYPE.input:
+            quizInput();
+            break;
+        case QUIZ_TYPE.multipy:
+            quizMultipy();
+            break;
+    }
 
-        button.classList.add("quiz-hide");
+    function quizSingle() {
+        const radio = document.querySelector(
+            "#" + formId + " .quiz-checkbox input[checkbox-success]"
+        );
 
-        if (radio.checked) {
-            alertSuccess.classList.remove("quiz-hide");
-        } else {
-            alertError.classList.remove("quiz-hide");
-        }
-    });
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            button.classList.add("quiz-hide");
+
+            if (radio.checked) {
+                alertSuccess.classList.remove("quiz-hide");
+            } else {
+                alertError.classList.remove("quiz-hide");
+            }
+        });
+    }
+
+    function quizInput() {
+        
+    }
+
+    function quizMultipy() {
+        const checkboxsError = document.querySelectorAll(
+            "#" + formId + " .quiz-checkbox input[checkbox-error]"
+        );
+        const checkboxsSuccess = document.querySelectorAll(
+            "#" + formId + " .quiz-checkbox input[checkbox-success]"
+        );
+        let statusOne,
+            statusSecond,
+            statusThird = true;
+
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            statusOne = checkboxsSuccess[0].checked;
+            statusSecond = checkboxsSuccess[1].checked;
+
+            checkboxsError.forEach((checkbox) => {
+                if (checkbox.checked) {
+                    statusThird = false;
+                }
+            });
+
+            button.classList.add("quiz-hide");
+
+            if (statusOne && statusSecond && statusThird) {
+                alertSuccess.classList.remove("quiz-hide");
+            } else {
+                alertError.classList.remove("quiz-hide");
+            }
+        });
+    }
 };
 
-quizForm("paris-town");
+const quizForms = document.querySelectorAll("[quiz-form]");
 
-// console.log(
-//     document.querySelectorAll("[quiz-form]")[0].getAttribute("form-type")
-// );
+quizForms.forEach((form) =>
+    quizForm(form.getAttribute("id"), form.getAttribute("form-type"))
+);
